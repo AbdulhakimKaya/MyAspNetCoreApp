2df168c32a3d69fe938a27900235e663264d1914
Abdulhakim KAYA <73741703+KAYA-99@users.noreply.github.com>
2022-11-19 19:20:51 +0300

    asp.net core 301 partial view/view component end

--- MyAspNetCoreApp.Web/Controllers/HomeController.cs
+++ MyAspNetCoreApp.Web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyAspNetCoreApp.Web.Models;
 using System.Diagnostics;
+using AutoMapper;
 using MyAspNetCoreApp.Web.Helpers;
+using MyAspNetCoreApp.Web.ViewModels;
 
 namespace MyAspNetCoreApp.Web.Controllers
 {
@@ -10,14 +12,34 @@ namespace MyAspNetCoreApp.Web.Controllers
         private readonly ILogger<HomeController> _logger;
         //Helper _helper; // interface kullanılmadığı durumlar için anca pek kullanılan bir yöntem değildir
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext context, IMapper mapper)
         {
             _logger = logger;
             //_helper = helper;
+            _context = context;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
+            var products = _context.Products.OrderByDescending(x => x.Id).Select(x =>
+                new ProductPartialViewModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    Stock = x.Stock,
+                }
+            ).ToList();
+
+            ViewBag.productListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
+
             return View();
         }
 
@@ -26,6 +48,21 @@ namespace MyAspNetCoreApp.Web.Controllers
             //var text = "Asp.net";
             //var upperText = _helper.Upper(text);
 
+            var products = _context.Products.OrderByDescending(x => x.Id).Select(x =>
+                new ProductPartialViewModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    Stock = x.Stock,
+                }
+            ).ToList();
+
+            ViewBag.productListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
+
             return View();
         }
 
@@ -33,6 +70,33 @@ namespace MyAspNetCoreApp.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Visitor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveVisitorComment(VisitorViewModel visitorViewModel)
+        {
+            try
+            {
+                var visitor = _mapper.Map<Visitor>(visitorViewModel);
+                visitor.Created = DateTime.Now;
+                _context.Visitors.Add(visitor);
+                _context.SaveChanges();
+
+                TempData["result"] = "comment saved";
+
+                return RedirectToAction(nameof(HomeController.Visitor));
+            }
+            catch (Exception)
+            {
+                TempData["result"] = "comment can not saved";
+
+                return RedirectToAction(nameof(HomeController.Visitor));
+            }
         }
     }
 }
--- MyAspNetCoreApp.Web/Models/AppDbContext.cs
+++ MyAspNetCoreApp.Web/Models/AppDbContext.cs
@@ -10,5 +10,6 @@ namespace MyAspNetCoreApp.Web.Models
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Visitor> Visitors { get; set; }
     }
 }
